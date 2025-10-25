0e97ca2cd48ced37caedd477ac38677823f79e7a
Daniel <codedbydaniel@gmail.com>
2025-10-25 14:44:37 +0630

    create

--- src/pages/ProductCreate.jsx
+++ src/pages/ProductCreate.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const ProductCreate = () => {
     return (
-        <div>ProductCreate</div>
+        <div>
+            <h1 className='text-center text-2xl mb-10'>Product Create Form</h1>
+            <form className='grid grid-cols-1 gap-4 max-w-xl mx-auto'>
+                <input placeholder='Enter product name' className='border border-solid border-black px-6 py-4 rounded-md text-base' type="text" />
+                <input placeholder='Enter product price' className='border border-solid border-black px-6 py-4 rounded-md text-base' type='number' />
+                <button className='bg-red-600 px-6 py-4 rounded-md text-white hover:bg-red-700 cursor-pointer text-lg' type='submit'>Create</button>
+            </form>
+        </div>
     )
 }
 
