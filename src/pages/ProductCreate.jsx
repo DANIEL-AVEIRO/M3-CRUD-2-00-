4e4109ab174c5fea8f54a854f9b2d450d78b92a2
Daniel <codedbydaniel@gmail.com>
2025-10-25 15:21:47 +0630

    create

--- src/pages/ProductCreate.jsx
+++ src/pages/ProductCreate.jsx
@@ -1,16 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 const ProductCreate = () => {
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!name || !price) {
+            toast.error("All fields are required");
+        }
+
+        const storeData = JSON.parse(localStorage.getItem("products")) || []
+        const newProduct = {
+            id: Date.now(),
+            name,
+            price,
+        }
+        storeData.push(newProduct)
+        localStorage.setItem("products", JSON.stringify(storeData))
+    };
     return (
         <div>
-            <h1 className='text-center text-2xl mb-10'>Product Create Form</h1>
-            <form className='grid grid-cols-1 gap-4 max-w-xl mx-auto'>
-                <input placeholder='Enter product name' className='border border-solid border-black px-6 py-4 rounded-md text-base' type="text" />
-                <input placeholder='Enter product price' className='border border-solid border-black px-6 py-4 rounded-md text-base' type='number' />
-                <button className='bg-red-600 px-6 py-4 rounded-md text-white hover:bg-red-700 cursor-pointer text-lg' type='submit'>Create</button>
+            <h1 className="text-center text-2xl mb-10">Product Create Form</h1>
+            <form
+                onSubmit={handleSubmit}
+                className="grid grid-cols-1 gap-4 max-w-xl mx-auto"
+            >
+                <input
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Enter product name"
+                    className="border border-solid border-black px-6 py-4 rounded-md text-base"
+                    type="text"
+                />
+                <input
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    placeholder="Enter product price"
+                    className="border border-solid border-black px-6 py-4 rounded-md text-base"
+                    type="number"
+                />
+                <button
+                    className="bg-red-600 px-6 py-4 rounded-md text-white hover:bg-red-700 cursor-pointer text-lg"
+                    type="submit"
+                >
+                    Create
+                </button>
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default ProductCreate
+export default ProductCreate;
